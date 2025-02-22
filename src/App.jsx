05a8d1339a0ee016822d5c03a831279008abd403
JorgeNeira <jorgeneira2006@gmail.com>
2025-02-22 07:45:22 +0100

    Product implementation and renamed files

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ export default function App() {
       <Routes>
         <Route index element={<Home />} />
         <Route path="/home" element={<Home />} />
-
       </Routes>
     </BrowserRouter>
   );
